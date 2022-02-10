462564a4caa3773d207ce0a7c2fabeae7eba897a
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-02-10 13:46:51 +0500

    spp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';    
 import '../css/header.scss';
 
 const Header = () => {
   return <>
     <div className="header d-flex align-items-end justify-content-center position-relative">
         <div className="container">
-            <div className="header-btn"><Link className="btn myBtn">Связаться с нами</Link></div>
+            <div className="header-btn"><a href='tel: +998943698058' className="btn myBtn">Связаться с нами</a></div>
         </div>
       {/* <div className="img"><img src="/img/header-bg.png" alt="" /></div> */}
     </div>
